77e9cbe8a97ef378f9cc198b8dedcb476879406e
Aadi1234567 <80298403+Aadi1234567@users.noreply.github.com>
2021-06-06 15:12:40 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,targetleft,targetright,targetmiddle;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	
+	targetmiddle=createSprite(400,660,200,20);
+	targetmiddle.shapeColor=color("red");
+	targetmiddle=Bodies.rectangle(400,600,200,20,{isStatic: true});
+	
+	
+	
+	targetleft=createSprite(300,620,20,100);
+	targetleft.shapeColor=color("red");
+	targetleft=Bodies.rectangle(300,620,20,100,{isStatic: true});
+
+	targetright=createSprite(500,620,20,100);
+	targetright.shapeColor=color("red");
+	targetright=Bodies.rectangle(500,600,20,100,{isStatic: true});
+	
+
+
+
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+	packageSprite.isStatic=false;
+
+
+
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
+
+
+
